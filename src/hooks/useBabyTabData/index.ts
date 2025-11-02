159ed7d210953a0d5229a0b6b8033faf131e4f2e
Luke-Michael-Brown <luke.michael.brown@gmail.com>
2025-11-02 10:14:41 -0500

    Remove seconds

--- src/hooks/useBabyTabData/index.ts
+++ src/hooks/useBabyTabData/index.ts
@@ -4,6 +4,16 @@ import useGoogleAPI from '../useGoogleAPI';
 import selectedTabAtom, { TABS } from '../../atoms/selectedTabAtom';
 import selectedBabyAtom from '../../atoms/selectedBabyAtom';
 
+const DATE_OPTIONS = {
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
+  hour: '2-digit',
+  minute: '2-digit',
+  second: undefined,
+  hour12: true,
+};
+
 export default function useBabyTabData() {
   const { fetchJsonFromDrive } = useGoogleAPI();
   const selectedTab = useAtomValue(selectedTabAtom);
@@ -20,8 +30,8 @@ export default function useBabyTabData() {
         .sort((a, b) => new Date(b.start_time).getTime() - new Date(a.start_time).getTime())
         .map((entry) => ({
           ...entry,
-          start_time: new Date(entry.start_time).toLocaleString(),
-          end_time: new Date(entry.end_time).toLocaleString(),
+          start_time: new Date(entry.start_time).toLocaleString('en-US', DATE_OPTIONS),
+          end_time: new Date(entry.end_time).toLocaleString('en-US', DATE_OPTIONS),
         }));
     },
   });
