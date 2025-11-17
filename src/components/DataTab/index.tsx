74c105a372f06b0450b9e0fad428009f9f10f026
Luke-Michael-Brown <luke.michael.brown@gmail.com>
2025-11-16 19:02:01 -0500

    Increase page size

--- src/components/DataTab/index.tsx
+++ src/components/DataTab/index.tsx
@@ -3,7 +3,7 @@ import { DataGrid } from "@mui/x-data-grid";
 import useBabyTabData from "../../hooks/useBabyTabData";
 import selectedTabAtom, { TABS, COLUMNS } from "../../atoms/selectedTabAtom";
 
-const paginationModel = { page: 0, pageSize: 8 };
+const paginationModel = { page: 0, pageSize: 10 };
 
 function DataTab() {
 	const selectedTab = useAtomValue(selectedTabAtom);
