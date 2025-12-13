85701318c07b2af3be5a96b3ffad1fb411fce279
Luke-Michael-Brown <luke.michael.brown@gmail.com>
2025-12-13 10:22:59 -0500

    Update DataTab to use config

--- src/config.ts
+++ src/config.ts
@@ -8,177 +8,175 @@ import JoinInnerIcon from '@mui/icons-material/JoinInner'
 import type { SvgIconComponent } from '@mui/icons-material'
 import type { PaletteColorOptions } from '@mui/material'
 
-type COLUMN_ENTRY = GridColDef & {
-  entryConfig?: { formType?: string } | { formType: 'select'; selectFields: string[] }
-}
+type FieldEntry =
+  | {
+      formType: string
+      selectFields?: never
+      columnFields: GridColDef
+    }
+  | {
+      formType: 'select'
+      selectFields: string[]
+      columnFields: GridColDef
+    }
 
 interface ConfigEntry {
   Icon: SvgIconComponent | typeof AppIcon
   lightPalette: PaletteColorOptions
   darkPalette: PaletteColorOptions
-  columns?: COLUMN_ENTRY[]
+  fields?: FieldEntry[]
 }
 
-const config: { [key: string]: ConfigEntry } = {
+const config: Record<string, ConfigEntry> = {
   summary: {
     Icon: AppIcon,
     lightPalette: { main: '#4dabf5', contrastText: '#fff' },
     darkPalette: { main: '#42a5f5', contrastText: '#121212' },
   },
+
   bottle: {
     Icon: WaterDropIcon,
     lightPalette: { main: '#ffb74d', contrastText: '#121212' },
     darkPalette: { main: '#ffa726', contrastText: '#121212' },
-    columns: [
+    fields: [
       {
-        flex: 100,
-        field: 'start_time',
-        headerName: 'Start',
-        entryConfig: {
-          formType: 'datePicker',
+        formType: 'datePicker',
+        columnFields: {
+          field: 'start_time',
+          headerName: 'Start',
         },
       },
       {
-        flex: 60,
-        field: 'extra1',
-        headerName: 'mL',
-        entryConfig: {
-          formType: 'number',
+        formType: 'number',
+        columnFields: {
+          field: 'extra1',
+          headerName: 'mL',
         },
       },
       {
-        flex: 60,
-        field: 'extra2',
-        headerName: 'VitD',
-        entryConfig: {
-          formType: 'checkbox',
+        formType: 'checkbox',
+        columnFields: {
+          field: 'extra2',
+          headerName: 'VitD',
         },
       },
     ],
   },
+
   diaper: {
     Icon: BabyChangingStationIcon,
     lightPalette: { main: '#81c784', contrastText: '#fff' },
     darkPalette: { main: '#66bb6a', contrastText: '#121212' },
-    columns: [
+    fields: [
       {
-        flex: 100,
-        field: 'start_time',
-        headerName: 'Start',
-        entryConfig: {
-          formType: 'datePicker',
+        formType: 'datePicker',
+        columnFields: {
+          field: 'start_time',
+          headerName: 'Start',
         },
       },
       {
-        flex: 100,
-        field: 'extra1',
-        headerName: 'Type',
-        entryConfig: {
-          formType: 'select',
-          selectFields: ['Pee', 'Poo', 'Pee & Poo'],
+        formType: 'select',
+        selectFields: ['Pee', 'Poo', 'Pee & Poo'],
+        columnFields: {
+          field: 'extra1',
+          headerName: 'Type',
         },
       },
     ],
   },
+
   pump: {
     Icon: JoinInnerIcon,
     lightPalette: { main: '#e57373', contrastText: '#fff' },
     darkPalette: { main: '#ef5350', contrastText: '#ffffff' },
-    columns: [
+    fields: [
       {
-        flex: 100,
-        field: 'start_time',
-        headerName: 'Start',
-        entryConfig: {
-          formType: 'datePicker',
+        formType: 'datePicker',
+        columnFields: {
+          field: 'start_time',
+          headerName: 'Start',
         },
       },
       {
-        flex: 60,
-        field: 'extra2',
-        headerName: 'mL',
-        entryConfig: {
-          formType: 'number',
+        formType: 'number',
+        columnFields: {
+          field: 'extra2',
+          headerName: 'mL',
         },
       },
       {
-        flex: 60,
-        field: 'extra3',
-        headerName: 'Power',
-        entryConfig: {
-          formType: 'checkbox',
+        formType: 'checkbox',
+        columnFields: {
+          field: 'extra3',
+          headerName: 'Power',
         },
       },
     ],
   },
+
   nurse: {
     Icon: PregnantWomanIcon,
     lightPalette: { main: '#fff176', contrastText: '#121212' },
     darkPalette: { main: '#ffee58', contrastText: '#121212' },
-    columns: [
+    fields: [
       {
-        flex: 100,
-        field: 'start_time',
-        headerName: 'Start',
-        entryConfig: {
-          formType: 'datePicker',
+        formType: 'datePicker',
+        columnFields: {
+          field: 'start_time',
+          headerName: 'Start',
         },
       },
       {
-        flex: 100,
-        field: 'end_time',
-        headerName: 'End',
-        entryConfig: {
-          formType: 'datePicker',
+        formType: 'datePicker',
+        columnFields: {
+          field: 'end_time',
+          headerName: 'End',
         },
       },
       {
-        flex: 100,
-        field: 'extra1',
-        headerName: 'Side',
-        entryConfig: {
-          formType: 'select',
-          selectFields: ['Left', 'Right', 'Both'],
+        formType: 'select',
+        selectFields: ['Left', 'Right', 'Both'],
+        columnFields: {
+          field: 'extra1',
+          headerName: 'Side',
         },
       },
       {
-        flex: 90,
-        field: 'extra2',
-        headerName: 'VitD',
-        entryConfig: {
-          formType: 'checkbox',
+        formType: 'checkbox',
+        columnFields: {
+          field: 'extra2',
+          headerName: 'VitD',
         },
       },
     ],
   },
+
   sleep: {
     Icon: CribIcon,
     lightPalette: { main: '#b39ddb', contrastText: '#fff' },
     darkPalette: { main: '#9575cd', contrastText: '#121212' },
-    columns: [
+    fields: [
       {
-        flex: 100,
-        field: 'start_time',
-        headerName: 'Start',
-        entryConfig: {
-          formType: 'datePicker',
+        formType: 'datePicker',
+        columnFields: {
+          field: 'start_time',
+          headerName: 'Start',
         },
       },
       {
-        flex: 100,
-        field: 'end_time',
-        headerName: 'End',
-        entryConfig: {
-          formType: 'datePicker',
+        formType: 'datePicker',
+        columnFields: {
+          field: 'end_time',
+          headerName: 'End',
         },
       },
       {
-        flex: 100,
-        field: 'extra1',
-        headerName: 'Type',
-        entryConfig: {
-          formType: 'select',
-          selectFields: ['Night Sleep', 'Nap'],
+        formType: 'select',
+        selectFields: ['Night Sleep', 'Nap'],
+        columnFields: {
+          field: 'extra1',
+          headerName: 'Type',
         },
       },
     ],
